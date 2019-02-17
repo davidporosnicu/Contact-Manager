9a94eb80c5e0c04e0fb1f03b74275f6898208555
David Porosnicu <david94_p@yahoo.com>
2019-02-17 14:42:28 +0200

    add Contact comp

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Contact from './Contact'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>The App Component</h1>
+        <Contact />
+      </div>
+    )
+  }
+}
+
+export default App
