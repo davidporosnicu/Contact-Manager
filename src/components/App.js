c2d6f3b56302ca798d78f74e5056730bb13fa91b
David Porosnicu <david94_p@yahoo.com>
2019-02-17 16:40:32 +0200

    add Contacts comp (hardcoded state)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
-import Contact from './Contact'
+import Contacts from './Contacts'
+import Header from './Header'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render () {
     return (
       <div>
-        <h1>The App Component</h1>
-        <Contact />
+        <Header branding='Contact Manager' />
+        <div className='container'>
+          <Contacts />
+        </div>
       </div>
     )
   }
---
+++ src/components/Contacts.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import Contact from './Contact'
+
+class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '312-345-6789'
+      },
+      {
+        id: 2,
+        name: 'Karen Williams',
+        email: 'karen@gmail.com',
+        phone: '312-555-6781'
+      },
+      {
+        id: 3,
+        name: 'Henry Johnson',
+        email: 'henry@gmail.com',
+        phone: '712-345-9999'
+      }
+    ]
+  }
+
+  render () {
+    const { contacts } = this.state
+    return (
+      <div>
+        {contacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default Contacts
