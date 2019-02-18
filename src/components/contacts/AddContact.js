ab55e5d6ea39edd8a14e421f90c7492fe88f33dc
David Porosnicu <david94_p@yahoo.com>
2019-02-18 12:21:39 +0200

    add Routes (+ NotFound comp & About comp)

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -44,6 +44,8 @@ class AddContact extends Component {
       phone: '',
       errors: {}
     })
+
+    this.props.history.push('/')
   }
 
   onInputChange = e => this.setState({ [e.target.name]: e.target.value })
